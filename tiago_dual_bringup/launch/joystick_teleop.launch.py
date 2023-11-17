5eab5464e3b6934bbbcbefbb0e35fc8f400adef6
David ter Kuile <david.terkuile@pal-robotics.com>
2023-11-17 10:55:46 +0100

    Add modules for module manager

--- tiago_dual_bringup/launch/joystick_teleop.launch.py
+++ tiago_dual_bringup/launch/joystick_teleop.launch.py
@@ -20,6 +20,7 @@ from launch import LaunchDescription
 from launch.actions import Node, DeclareLaunchArgument, OpaqueFunction
 from launch.conditions import LaunchConfigurationEquals
 from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 from launch_pal.arg_utils import read_launch_argument
 
@@ -80,18 +81,18 @@ def declare_launch_arguments(launch_description: LaunchDescription):
     launch_description.add_action(end_effector_left)
 
     ft_sensor_right = DeclareLaunchArgument(
-            'ft_sensor_right',
-            default_value='schunk-ft',
-            description='FT sensor model. ',
-            choices=['schunk-ft', 'no-ft-sensor'])
+        'ft_sensor_right',
+        default_value='schunk-ft',
+        description='FT sensor model. ',
+        choices=['schunk-ft', 'no-ft-sensor'])
 
     launch_description.add_action(ft_sensor_right)
 
     ft_sensor_left = DeclareLaunchArgument(
-            'ft_sensor_left',
-            default_value='schunk-ft',
-            description='FT sensor model. ',
-            choices=['schunk-ft', 'no-ft-sensor'])
+        'ft_sensor_left',
+        default_value='schunk-ft',
+        description='FT sensor model. ',
+        choices=['schunk-ft', 'no-ft-sensor'])
 
     launch_description.add_action(ft_sensor_left)
 
@@ -101,7 +102,8 @@ def declare_launch_arguments(launch_description: LaunchDescription):
 
     launch_description.add_action(cmd_val)
 
-    launch_description.add_action(OpaqueFunction(function=create_joy_teleop_filename))
+    launch_description.add_action(OpaqueFunction(
+        function=create_joy_teleop_filename))
 
     return
 
@@ -109,10 +111,10 @@ def declare_launch_arguments(launch_description: LaunchDescription):
 def declare_actions(launch_description: LaunchDescription):
 
     joy_teleop_node = Node(
-       package='joy_teleop',
-       executable='joy_teleop',
-       parameters=[LaunchConfiguration('teleop_config')],
-       remappings=[('cmd_vel', LaunchConfiguration('cmd_vel'))])
+        package='joy_teleop',
+        executable='joy_teleop',
+        parameters=[LaunchConfiguration('teleop_config')],
+        remappings=[('cmd_vel', LaunchConfiguration('cmd_vel'))])
 
     launch_description.add_action(joy_teleop_node)
 
@@ -156,13 +158,13 @@ def declare_actions(launch_description: LaunchDescription):
 
 def create_joy_teleop_filename(context):
     hw_suffix = get_tiago_dual_hw_suffix(
-                arm_right=read_launch_argument('arm_type_right', context),
-                arm_left=read_launch_argument('arm_type_left', context),
-                end_effector_right=read_launch_argument('end_effector_right', context),
-                end_effector_left=read_launch_argument('end_effector_left', context),
-                ft_sensor_right=read_launch_argument('ft_sensor_right', context),
-                ft_sensor_left=read_launch_argument('ft_sensor_left', context),
-            )
+        arm_right=read_launch_argument('arm_type_right', context),
+        arm_left=read_launch_argument('arm_type_left', context),
+        end_effector_right=read_launch_argument('end_effector_right', context),
+        end_effector_left=read_launch_argument('end_effector_left', context),
+        ft_sensor_right=read_launch_argument('ft_sensor_right', context),
+        ft_sensor_left=read_launch_argument('ft_sensor_left', context),
+    )
 
     joy_teleop_file = f"joy_teleop_{hw_suffix}.yaml"
 
@@ -170,7 +172,7 @@ def create_joy_teleop_filename(context):
         get_package_share_directory('tiago_dual_bringup'), 'config', 'joy_teleop', joy_teleop_file)
 
     joy_teleop_config = DeclareLaunchArgument(
-       'teleop_config', default_value=joy_teleop_path,
-       description='Joystick teleop configuration file')
+        'teleop_config', default_value=joy_teleop_path,
+        description='Joystick teleop configuration file')
 
     return [joy_teleop_config]
